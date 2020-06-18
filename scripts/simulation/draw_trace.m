73a96fdb84e7f26886f389d3ff204107b73c936e
Rhiju Das <rhiju@stanford.edu>
2020-06-18 09:54:10 -0700

    inline attack runs & notes

--- scripts/simulation/draw_trace.m
+++ scripts/simulation/draw_trace.m
@@ -1,28 +1,35 @@
-function draw_trace( t, R );
-% draw_trace( y );
-% draw_trace( t, R );
+function draw_trace( y, step_types );
+% draw_trace( y, step_types );
 %
 % Draw trace with coordinate frames.
 %
-% Input can be
-%
+% Input should be
+% y = struct with:
 %  t = [3 x N] coordinates of a  trace
-%  R   = [3 x 3 x N] orthonormal frames of a  trace
-%
-% Or it can be
-%
-% y = struct with above fields.
+%  R = [3 x 3 x N] orthonormal frames of a  trace
 %
 % (C) R. Das, Stanford 2020
 
-if isstruct( t );
-    y = t;
-    t = y.t;
-    R = y.R;
+t = y.t;
+R = y.R;
+
+if ~exist( 'step_types', 'var' )
+    % plot trace as usual.
+    plot3( t(1,:), t(2,:),t(3,:),'linew',2 ); hold on
 end
 
-plot3( t(1,:), t(2,:),t(3,:),'linew',2 ); hold on
 for n = 1:size( t, 2 );
+    if exist( 'step_types', 'var' )
+        % Draw trace, with colorcoding of types of transforms.
+        if n < size( t, 2 )
+            h = plot3( t(1,n+[0,1]), t(2,n+[0,1]),t(3,n+[0,1]),'linew',2 ); hold on
+            colorcode = [0,0,1];
+            if strcmp(step_types{n},'Inline') colorcode = [1,0,0]; end;
+            set(h,'color',colorcode );
+        end
+    end
+    
+    % Draw coordinate frame
     xvec = [t(:,n), t(:,n) + 3*R(:,1,n)];
     yvec = [t(:,n), t(:,n) + 3*R(:,2,n)];
     zvec = [t(:,n), t(:,n) + 3*R(:,3,n)];
