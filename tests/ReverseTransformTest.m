8739fd3c824a8f8b70296d4855ef1cba50f3218e
Rhiju Das <rhiju@stanford.edu>
2020-06-18 08:37:04 -0700

    move reverse_transform_test.m into a unit test tests/ directory

---
+++ tests/ReverseTransformTest.m
@@ -0,0 +1,14 @@
+pdbstruct = pdbread( 'example_data/4ybb_DIII_just3nts.pdb');
+start_triad = {'C5''','C4''','C3'''};
+[ctr,M,ok] = get_frames( pdbstruct, start_triad );
+ctr1 = ctr(:,1);
+ctr2 = ctr(:,2);
+M1 = M(:,:,1);
+M2 = M(:,:,2);
+
+[tf,Rf] = get_transform( ctr1, M1, ctr2, M2);
+[tr,Rr] = get_transform( ctr2, M2, ctr1, M1);
+[tf_rev, Rf_rev] =  reverse_transform( tf,Rf);
+
+assert( all(abs(tf_rev - tr)<1e-5) )
+assert( all(abs(Rf_rev(:) - Rr(:))<1e-5) )
